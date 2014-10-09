cc6c23a08fbb90ab8cade8386ba4fe10b92ca004
bjmayor <415074476@qq.com>
2014-10-10 00:37:35 +0800

    调整成Android studio开发模式。

--- src/com/hiwifi/activity/test/TestCenterActivity.java
+++ src/com/hiwifi/activity/test/TestCenterActivity.java
@@ -47,7 +47,6 @@ public class TestCenterActivity extends FragmentActivity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 
